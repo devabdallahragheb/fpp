346ae6cb8da53940cc285185bc834f564ebdbef7
aragheb <abdallah.ragheb@squadio.com>
2024-05-28 04:18:38 +0300

    new comment

--- untitled/src/HomeWork1/Prog1.java
+++ untitled/src/HomeWork1/Prog1.java
@@ -4,9 +4,11 @@ import java.util.Random;
 
 public class Prog1 {
     public static void main(String[]args){
+        // get random number
         Random rand = new Random();
         int x = rand.nextInt(9) + 1;
         int y = rand.nextInt(14 - 3 + 1) + 3;
+
         System.out.printf(" %.2f\n", Math.PI * x);
         System.out.printf("%.2f",  y * Math.PI);
     }
