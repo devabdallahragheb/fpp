4aa7f0312835a269bd36f05aff3440ea7c7d16fa
aragheb <abdallah.ragheb@squadio.com>
2024-05-30 04:42:35 +0300

    homework

---
+++ fpp/src/HomeWork2/Prog4.java
@@ -0,0 +1,33 @@
+package HomeWork2;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Prog4 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(distinctWords(new String[]{"horse", null, "cat", "horse", "dog"})));
+    }
+
+    public static String[] distinctWords(String[] input) {
+        String[] distinctWords = new String[input.length];
+        int counter = 0;
+        for (String s : input) {
+            if(s == null) continue;
+            boolean duplicate = false;
+            for (int j = 0; j < counter; j++)
+                if (distinctWords[j].equals(s)) {
+                    duplicate = true;
+                    break;
+                }
+            if (!duplicate) {
+                distinctWords[counter] = s;
+                counter++;
+            }
+        }
+
+        String[] result = new String[counter];
+        System.arraycopy(distinctWords, 0, result, 0, counter);
+
+        return result;
+    }
+}
---
+++ fpp/src/HomeWork2/Prog6.java
@@ -0,0 +1,26 @@
+package HomeWork2;
+
+public class Prog6 {
+    public static void main(String[] args) {
+        secondMin(new int[]{2, 21, 3, 45, 0, 12, 18, 6, 3, 1, 0, 22});
+    }
+
+    public static void secondMin(int[] arrayOfInts) {
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+        for (int i = 0; i < arrayOfInts.length; i++) {
+            if (arrayOfInts[i] < min) {
+                secondMin = min;
+                min = arrayOfInts[i];
+            } else if (arrayOfInts[i] > min && arrayOfInts[i] < secondMin) {
+                secondMin = arrayOfInts[i];
+            }
+        }
+
+        if (secondMin == min) {
+            System.out.println("There is no second min for this array");
+        } else {
+            System.out.printf("Second Min: %d \n", secondMin);
+        }
+    }
+}
