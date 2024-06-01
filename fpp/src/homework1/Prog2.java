2fec274f13242676db36066f33358b7fce52d6ca
aragheb <abdallah.ragheb@squadio.com>
2024-06-01 08:35:10 +0300

    new

--- fpp/src/homework1/Prog2.java
+++ fpp/src/homework1/Prog2.java
@@ -5,7 +5,6 @@ public class Prog2 {
     public static void main(String[] args) {
         float num1 = 1.27f,num2 = 3.881f,  num3 = 9.6f;
         float sumNumbers =  num1 + num2 + num3;
-        // sum number of float
         System.out.println((int) sumNumbers);
         System.out.println(Math.round(sumNumbers));
     }
