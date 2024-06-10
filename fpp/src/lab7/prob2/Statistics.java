3b7911162942c7d8f2e7cca2fcc186afe4d52106
Huynh Anh Vu Nguyen (Cris) <huynhanhvu.nguyen@miu.edu>
2024-06-10 14:44:13 -0500

    Complete Prob 2

--- fpp/src/lab7/prob2/Statistics.java
+++ fpp/src/lab7/prob2/Statistics.java
@@ -1,14 +1,21 @@
 package lab7.prob2;
 
-import java.util.*;
+import java.util.List;
+
 public class Statistics {
-	/** 
-	 * Polymorphically computes and returns the sum
-	 * of all the salaries of all the staff/teachers in the list.
-	 */
-	public static double computeSumOfSalaries(List/*<implement>*/ aList) {
-		return 0.0;
-		//implement
-		//compute sum of all salaries of people in aList and return
-	}
+    /**
+     * Polymorphically computes and returns the sum
+     * of all the salaries of all the staff/teachers in the list.
+     */
+    public static double computeSumOfSalaries(List<EmployeeData> employeeData) {
+        double sum = 0;
+        if (employeeData == null) {
+            return sum;
+        }
+        for (EmployeeData employee : employeeData) {
+            if (employee != null)
+                sum += employee.getSalary();
+        }
+        return sum;
+    }
 }
